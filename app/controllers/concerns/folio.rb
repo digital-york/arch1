f27f7733b0b45927f85fa1a8954b825fc330725d
Paul Young <paul.young@york.ac.uk>
2015-04-27 16:17:51 +0000

    Added comments and code tidy

--- app/controllers/concerns/folio.rb
+++ app/controllers/concerns/folio.rb
@@ -1,28 +1,46 @@
 module Folio
 
-def get_folios
-    @folios = []
-    File.readlines(ENV["FOLIO_LIST"].to_s).each do |line|
-      line = line.sub('Reg_12_', '').sub('.jp2', '')
-      line = remove_carriage_returns(line)
-      line_array = line.split("_")
-      folio = line_array[0]
-      folio_face = line_array[1]
-      @folios << 'Folio ' + folio + ' - ' + folio_face
+  # Get the first and last folio in the folio list and store as session variables so that the image
+  # '<' or '>' button is greyed out if folio is equal to the first or last one
+  def get_first_and_last_folio
+    folio_all_lines = get_folios
+    first_folio = folio_all_lines[0].strip.sub('.jp2', '').sub(' ', '_')
+    last_folio = folio_all_lines[folio_all_lines.length - 1].strip.sub('.jp2', '')
+    session[:first_folio] = first_folio
+    session[:last_folio] = last_folio
+  end
+
+  # Set the register, folio and folio face session variables (i.e. those chosen from the drop-down lists) for the current folio
+  # Note that the 'folio_choice' session variable contains both the folio and folio face (as it is used to remember the drop-down list choice)
+  # but the variables are stored separately as 'folio' and 'folio_face' session variables (which are then saved in Fedora for the particular entry)
+  def get_folio
+    session[:register_choice] = params[:register_choice].strip
+    session[:folio_choice] = params[:folio_choice].strip
+    register_params = params[:folio_choice] || ''
+    if register_params != ''
+      folio_array = register_params.split '-'
+      folio = folio_array[0].sub('Folio ', '').strip
+      folio_face = folio_array[1].strip.sub(' ', '_')
+      session[:folio] = folio
+      session[:folio_face] = folio_face
     end
+    # The image session variable defines the name of the jp2 image file in dlib
+    # 'Reg_12' will need changing eventually when there are more registers
+    session[:image] = 'Reg_12_' + session[:folio] + '_' + session[:folio_face] + '.jp2'
   end
 
   def get_next_image(type)
 
-    folio_all_lines = get_folios
-    number_of_folios = folio_all_lines.length
+    folio_all_lines = get_folios # Basically an array of all the lines in the text file, i.e. all the folio names
 
     current_folio = 'Reg_12_' + session[:folio] + '_' + session[:folio_face].sub('_', ' ') + '.jp2'
 
+    # Iterate over all the folios
     folio_all_lines.each_with_index do |folio_line, index|
 
-      folio_line = remove_carriage_returns(folio_line)
+      folio_line = folio_line.chop.strip # remove carriage return and spaces
 
+      # If the current folio matches with the line, find out the next folio name
       if folio_line == current_folio
 
         if type == '+'
@@ -32,7 +50,7 @@ def get_folios
         end
 
         # Set the image path
-        next_folio = remove_carriage_returns(next_folio)
+        next_folio = next_folio.chop.strip # remove carriage return and spaces
         session[:image] = next_folio.sub(' ', '_') # DZI image expects an underscore and not a space, e.g. 'Insert_a'
 
         # Set the session variables
@@ -48,31 +66,17 @@ def get_folios
     end
   end
 
-  def remove_carriage_returns(str)
-    str = str.strip
-    return str.sub(/\r/, " ").sub(/\n/, " ")
-  end
-
-  def get_folio
-    session[:register_choice] = params[:register_choice].strip
-    session[:folio_choice] = params[:folio_choice].strip
-    register_params = params[:folio_choice] || ''
-    if register_params != ''
-      folio_array = register_params.split '-'
-      folio = folio_array[0].sub('Folio ', '').strip
-      folio_face = folio_array[1].strip.sub(' ', '_')
-      session[:folio] = folio
-      session[:folio_face] = folio_face
+  # Read the folio text file lines into an array
+  # Used in methods above and to display the folio drop-down list on the view pages
+  def get_folios
+    @folios = []
+    File.readlines(ENV["FOLIO_LIST"].to_s).each do |line|
+      line = line.sub('Reg_12_', '').sub('.jp2', '')
+      line = line.chop.strip # remove carriage return and spaces
+      line_array = line.split("_")
+      folio = line_array[0]
+      folio_face = line_array[1]
+      @folios << 'Folio ' + folio + ' - ' + folio_face
     end
-    session[:image] = 'Reg_12_' + session[:folio] + '_' + session[:folio_face] + '.jp2'
   end
-
-  def get_first_and_last_folio
-    folio_all_lines = get_folios
-    first_folio = folio_all_lines[0].strip.sub('.jp2', '').sub(' ', '_')
-    last_folio = folio_all_lines[folio_all_lines.length - 1].strip.sub('.jp2', '')
-    session[:first_folio] = first_folio
-    session[:last_folio] = last_folio
-  end
-
 end
--- app/controllers/entries_controller.rb
+++ app/controllers/entries_controller.rb
@@ -1,5 +1,6 @@
 class EntriesController < ApplicationController
 
+  # Set the entry number, get the folio list (for the drop-down) and check if the session is timed out before calling the appropriate methods
   before_action :set_entry, only: [:index, :show, :edit, :update]
   before_action :get_folios, only: [:index, :show, :new, :edit, :create, :update, :destroy]
   before_filter :session_timed_out, except: [:login]
@@ -7,6 +8,9 @@ class EntriesController < ApplicationController
   # NOTE - want to display folio with a space, e.g. 'Insert a' but save the field as 'Insert_a' because
   # there was a problem with the 'Entry.where' statement when a space occurred - not sure why
 
+  # Check the :login variable in the session to see if the session has timed out
+  # If so, render 'timed_out.erb'
+  # Note that the session timeout value is set in config/initializers/session_store.rb
   def session_timed_out
     if session[:login] == '' || session[:login] == nil
       render 'timed_out', :layout => 'session_timed_out'
@@ -14,47 +18,47 @@ class EntriesController < ApplicationController
   end
 
   def timed_out
-    # hopefully render the timed_out page
+    # Hopefully render the timed_out page (see render 'timed_out' above)
   end
 
   # LOGOUT
+  # Make sure the session variables are all made equal to '' and redirect to the login page
   def logout
     session[:register_choice] = ''
     session[:folio_choice] = ''
     session[:folio] = ''
     session[:folio_face] = ''
     session[:login] = ''
+    session[:first_folio] = ''
+    session[:last_folio] = ''
     redirect_to login_path, :layout => 'login'
   end
 
   # LOGIN
   def login
+
+    # If request is from the login page do this...
     if params[:submit] == 'true'
 
+      # Simple test for username/password (hard-coded - this will be changed to proper authorisation later on)
       if params[:username] == 'test' && params[:password] == 'test'
-        session[:login] = 'true'
+        session[:login] = 'true' # This is the session login token which we can test in the 'before_filter' method at the top
+        get_first_and_last_folio # This is needed for the image '<' and '>' buttons
         redirect_to :action => 'index'
       else
         @error = 'true'
         render :layout => 'login'
       end
+      # Else request has come from the session timeout page so do this...
     else
-      session[:register_choice] = ''
-      session[:folio_choice] = ''
-      session[:folio] = ''
-      session[:folio_face] = ''
-      session[:login] = ''
-      get_first_and_last_folio
       render :layout => 'login'
     end
   end
 
   # INDEX
   def index
-    
-	puts "INDEX"
-	
-    # Set the appropriate session variables when the register, folio and side are chosen and the 'Go' button is clicked
+
+    # Set the appropriate session variables when the register, folio and folio face are chosen and the 'Go' button is clicked
     if params[:go] == 'true'
       get_folio
     end
@@ -66,15 +70,15 @@ class EntriesController < ApplicationController
         get_next_image(params[:change_folio])
       end
 
-      # Get the first entry for the folio, else get the entry with the appropriate id
+      # Get the first entry for the folio if there isn't an id, else get the entry with the appropriate id
       if params[:id] == nil || params[:id] == ''
         @entry = Entry.where(:folio => session[:folio]).where(:folio_face => session[:folio_face]).first
+        # NOTE - had real problems with the above when there was a space in the 'folio_face', e.g. 'Insert a'; therefore saving to Fedora with an underscore
       else
         @entry = Entry.find(params[:id])
       end
 
-      # Get all the entries which match with the chosen register, folio and face
-      # NOTE - had real problems with above command when there is a space in the 'folio_face', e.g. 'Insert a', therefore saving with an underscore
+      # Get all the entries which match with the chosen register, folio and folio face
       @entries = Entry.where(:folio => session[:folio]).where(:folio_face => session[:folio_face])
     end
   end
@@ -88,11 +92,12 @@ class EntriesController < ApplicationController
   # NEW
   def new
 
-    # This code gets the new session variables and redirects to index if the 'Go' button is clicked
+    # This code sets the new session variables and redirects to index if the 'Go' button is clicked on the new page
     if params[:go] == 'true'
       get_folio
       redirect_to :action => 'index'
     end
+
     @entry = Entry.new
     @entries = Entry.where(:folio => session[:folio]).where(:folio_face => session[:folio_face])
     get_authority_lists
@@ -100,13 +105,13 @@ class EntriesController < ApplicationController
 
   # EDIT
   def edit
-  
-    # This code gets the new session variables and redirects to index if the 'Go' button is clicked
+
+    # This code sets the new session variables and redirects to index if the 'Go' button is clicked on the edit page
     if params[:go] == 'true'
       get_folio
       redirect_to :action => 'index'
     end
-	
+
     @entries = Entry.all.where(:folio => session[:folio]).where(:folio_face => session[:folio_face])
     get_authority_lists
   end
@@ -114,39 +119,37 @@ class EntriesController < ApplicationController
   # CREATE
   def create
 
-    puts "CREATE"
-	puts "1"
-    puts entry_params
-	@errors = validate(entry_params)
-	puts "2"
-	puts entry_params
-	
+    # See validation.rb in /concerns
+    @errors = validate(entry_params)
+
+    # Get a new entry and replace values with the form parameters
     @entry = Entry.new(entry_params)
 
-	if @errors != '' && @errors != nil
-	  @entries = Entry.where(:folio => session[:folio]).where(:folio_face => session[:folio_face])
+    # If there are errors, go back to the 'new' page and display the errors, else go to the 'index' page
+    if @errors != '' && @errors != nil
+      @entries = Entry.where(:folio => session[:folio]).where(:folio_face => session[:folio_face])
       get_authority_lists
-	  render 'new'
-	else @entry.save
+      render 'new'
+    else
+      @entry.save
       redirect_to :action => 'index', :id => @entry.id
     end
   end
 
   # UPDATE
   def update
-  
-    puts "UPDATE"
-    puts entry_params
-  
+
+    # See validation.rb in /concerns
     @errors = validate(entry_params)
-    
-	if @errors != '' && @errors != nil
-	  @entries = Entry.where(:folio => session[:folio]).where(:folio_face => session[:folio_face])
+
+    # If there are errors, render the go back to the 'edit' page and display the errors, else go to the 'index' page
+    if @errors != '' && @errors != nil
+      @entries = Entry.where(:folio => session[:folio]).where(:folio_face => session[:folio_face])
       get_authority_lists
-	  @entry.attributes = entry_params # Updates the @entry with the appropriate attributes before rendering the 'edit' page
-	  render 'edit'
+      @entry.attributes = entry_params # Updates the @entry with the appropriate attributes before rendering the 'edit' page
+      render 'edit'
     else
-	  @entry.update(entry_params)
+      @entry.update(entry_params)
       redirect_to :action => 'index', :id => @entry.id
     end
   end
@@ -155,12 +158,13 @@ class EntriesController < ApplicationController
   def destroy
     @entry = Entry.find(params[:id])
     @entry.destroy
-	redirect_to entries_path,  notice: 'Project was successfully destroyed.'
+    redirect_to entries_path, notice: 'Project was successfully destroyed.'
   end
 
-  # PRIVATE
+  # PRIVATE METHODS
   private
-  # Use callbacks to share common setup or constraints between actions.
+
+  # Get the entry for the specified id
   def set_entry
     if params[:id] != nil && params[:id] != ''
       @entry = Entry.find(params[:id])
@@ -169,8 +173,8 @@ class EntriesController < ApplicationController
 
   # Never trust parameters from the scary internet, only allow the white list through.
   def entry_params
-    params.require(:entry).permit!
+    params.require(:entry).permit! # Note - this needs changing because it allows through all params at the moment!!
     #params.require(:entry).permit(:entry_no, :access_provided_by, editorial_notes_attributes: [:id, :editorial_note, :_destroy], people_attributes: [:id, :name_as_written, :note, :age, :gender, :name_authority, :_destroy])
   end
-    
+
 end
