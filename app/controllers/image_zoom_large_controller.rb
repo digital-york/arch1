ed009d72da606e6414da46525a3b7eaf04e8b8c0
Paul Young <paul.young@york.ac.uk>
2015-04-28 11:19:51 +0000

    General code tidy-up
    Former-commit-id: e8beb10e2afaace32861177c6ab17556b3dc5ce8

---
+++ app/controllers/image_zoom_large_controller.rb
@@ -0,0 +1,30 @@
+class ImageZoomLargeController < ApplicationController
+
+  layout 'image_zoom_large'
+
+  require 'net/http'
+
+  before_filter :session_timed_out
+
+  def session_timed_out
+    if session[:login] == '' || session[:login] == nil
+      render 'timed_out', :layout => 'session_timed_out'
+    end
+  end
+
+  def index
+
+    remote_server = "http://dlib.york.ac.uk"
+    remote_dzi_base_url = '/cgi-bin/iipsrv.fcgi?DeepZoom=/usr/digilib-webdocs/digilibImages/ArchbishopsRegisters/'
+    remote_dzi_image_url = 'reg12/JP2/Reg_12_005_Recto.jp2.dzi'
+
+    @dzi_url_str = remote_server + remote_dzi_base_url + remote_dzi_image_url
+
+    # Get the xml from the url and replace the carraiage returns, otherwise it doesn't work
+    dzi_xml = Net::HTTP.get('dlib.york.ac.uk', remote_dzi_base_url + remote_dzi_image_url)
+    dzi_xml = dzi_xml.gsub(/\r/,' ')
+    @dzi_xml = dzi_xml.gsub(/\n/,' ')
+    @dzi_xml = @dzi_xml.html_safe
+  end
+
+end
