28a1391b6d996ec56d818a81662a2f90a1c11c58
Paul Young <paul.young@york.ac.uk>
2015-04-30 14:36:38 +0000

    Various validation changes
    Former-commit-id: 835e6d6b63b1301a4910aef496a1ec0352e4c880

--- app/assets/javascripts/entry.js
+++ app/assets/javascripts/entry.js
@@ -1,4 +1,4 @@
-// DatePickler function
+// datepicker
 $(function() {
         $('.datePicker').datepicker({
             showOn: "button",
@@ -14,6 +14,20 @@ $(function() {
 	$(this).datepicker();
 });	
 
+// Hide any elements which have been removed by the user
+// as they will reappear on the page if an error occurs
+function hide_elements_when_errors() {
+    console.log("start");
+    $("input").each(function(index) {
+        console.log(index);
+        if (this.type === "hidden" && this.value === "true") {
+            var parentTag = $(this).parent().get(0).tagName;
+            $(this).parent().parent().css("display", "none");
+            //console.log("MATCH: id=" + this.id + ": name=" + this.name + ": value=" + this.value + ": type=" + this.type + ": parent=" + parentTag);
+        }
+    });
+    console.log("end");
+}
 
 // Display the image zoom popup
 function image_zoom_large_popup(page) {
@@ -22,34 +36,4 @@ function image_zoom_large_popup(page) {
     var top = 0;
     var left = (screen.width-popupWidth)/2;
     window.open(page, "myWindow", "status = 1, top = " + top + ", left = " + left + ", height = " + popupHeight + ", width = " + popupWidth + ", scrollbars=yes");
-}
-
-// Not being used at present
-function validateForm() {
-	// <%= form_for @entry, :html=>{:name=>"myForm",:onsubmit=>"return validateForm();"} do |f| %>
-	var v = document.getElementsByName("myForm");
-	var x = "";
-	console.log("start");
-	console.log(v);
-	for (var i = 0; i < v.length; i++) {
-		v2 = v[1];
-		for (var j = 0; j < v2.length; j++) {
-			var name = v2[j].name;
-			var val = v2[j].value;
-			if (name == 'entry[entry_no]') {
-				if (val == '') {
-					console.log('EMPTY!');
-					//console.log(v2[j].name);
-					//	x = x + v[i].value;
-				} else {
-					console.log("1" + val + "2");
-				}
-			}
-		}
-	}
-	console.log("end");
-	//event.preventDefault();
-	//var s = "false";
-	//alert(s);
-	return false;
 }
