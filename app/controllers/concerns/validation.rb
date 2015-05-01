5cd1179408126c2f989f653369891198a0948ac6
Paul Young <paul.young@york.ac.uk>
2015-05-01 13:51:15 +0000

    Changes to validation

--- app/controllers/concerns/validation.rb
+++ app/controllers/concerns/validation.rb
@@ -13,29 +13,25 @@ module Validation
     errors = ''
 
     # Check single fields, i.e. those which do not have a '+' button
-    errors = errors + validate_length_and_mandatory_field(entry_params[:entry_no], 'Entry No', SMALL_FIELD)
-    errors = errors + validate_length_and_mandatory_field(entry_params[:access_provided_by], 'Access Provided By', MEDIUM_FIELD)
-    errors = errors + validate_length_and_mandatory_field(entry_params[:document], 'Document', MEDIUM_FIELD)
-    errors = errors + validate_length(entry_params[:document_part], 'Document Part', MEDIUM_FIELD)
-    errors = errors + validate_length_and_mandatory_field(entry_params[:folio_type], 'Folio Type', 10)
-    errors = errors + validate_length_and_mandatory_field(entry_params[:folio_face_temp], 'Folio Face Temp', 10)
-    errors = errors + validate_length(entry_params[:entry_part], 'Entry Part', MEDIUM_FIELD)
-
-    # Note for the nested elements below, e.g. Editorial Note...
-    # If the record is empty and an id exists (i.e. the record is already in Fedora), we can delete it with '_destroy=1'
-    # Note however that if there isn't an id, the record hasn't been yet been saved in Fedora and we don't want to make '_destroy=1'
-    # because the model attribute ':reject_if => 'all_blank' (in entry.rb) won't work!
-    #
-    # Note that errors are written to the 'errors' variable which are then displayed on the '_form.html.erb' page
+    errors = errors + get_errors(entry_params[:entry_no], 'Entry No', SMALL_FIELD, 'M')
+    errors = errors + get_errors(entry_params[:access_provided_by], 'Access Provided By', MEDIUM_FIELD, 'M')
+    errors = errors + get_errors(entry_params[:document], 'Document', MEDIUM_FIELD, 'M')
+    errors = errors + get_errors(entry_params[:document_part], 'Document Part', MEDIUM_FIELD, '')
+    errors = errors + get_errors(entry_params[:folio_type], 'Folio Type', 10, 'M')
+    errors = errors + get_errors(entry_params[:folio_face_temp], 'Folio Face Temp', 10, 'M')
+    errors = errors + get_errors(entry_params[:entry_part], 'Entry Part', MEDIUM_FIELD, '')
 
     # Editorial Note
+    # If the record is empty and an id exists (i.e. the record is already in Fedora), we can delete it with '_destroy=1'
+    # Note however that if there isn't an id, the record hasn't been yet been saved in Fedora and we don't want to make '_destroy=1'
+    # because the model attribute ':reject_if => 'all_blank' (in entry.rb) won't work! The same goes for Format, Marginal Note, etc
     if entry_params[:editorial_notes_attributes] != nil
       entry_params[:editorial_notes_attributes].values.each do |t|
-        if t[:_destroy] != '1' && t[:_destroy] != 'true'
+        if t[:_destroy] != '1' && t[:_destroy] != 'true' # This code bypasses validation checking if the field is hidden (otherwise it will display any errors for the hidden element!)
           if t[:id] && t[:editorial_note] == ''
             t[:_destroy] = '1'
           end
-          errors = errors + get_errors(t[:editorial_note], 'Editorial Note', LARGE_FIELD, '')
+          errors = errors + get_errors(t[:editorial_note], 'Editorial Note', LARGE_FIELD, '') # errors are written to the 'errors' variable which is displayed on the '_form.html.erb' page
         end
       end
     end
@@ -43,60 +39,72 @@ module Validation
     # Format
     if entry_params[:formats_attributes] != nil
       entry_params[:formats_attributes].values.each do |t|
+        if t[:_destroy] != '1' && t[:_destroy] != 'true'
         if t[:id] && t[:format] == ''
           t[:_destroy] = '1'
         end
-        errors = errors + validate_length(t[:format], 'Format', MEDIUM_FIELD)
+        errors = errors + get_errors(t[:format], 'Format', MEDIUM_FIELD, '')
+        end
       end
     end
 
     # Marginal Note
     if entry_params[:marginal_notes_attributes] != nil
       entry_params[:marginal_notes_attributes].values.each do |t|
+        if t[:_destroy] != '1' && t[:_destroy] != 'true'
         if t[:id] && t[:marginal_note] == ''
           t[:_destroy] = '1'
         end
-        errors = errors + validate_length(t[:marginal_note], 'Marginal Note', LARGE_FIELD)
+        errors = errors + get_errors(t[:marginal_note], 'Marginal Note', LARGE_FIELD, '')
+          end
       end
     end
 
     # Is Referenced By
     if entry_params[:is_referenced_bies_attributes] != nil
       entry_params[:is_referenced_bies_attributes].values.each do |t|
+        if t[:_destroy] != '1' && t[:_destroy] != 'true'
         if t[:id] && t[:is_referenced_by] == ''
           t[:_destroy] = '1'
         end
-        errors = errors + validate_length(t[:is_referenced_by], 'Is Referenced By', MEDIUM_FIELD)
+        errors = errors + get_errors(t[:is_referenced_by], 'Is Referenced By', MEDIUM_FIELD, '')
+          end
       end
     end
 
     # Language
     if entry_params[:languages_attributes] != nil
       entry_params[:languages_attributes].values.each do |t|
+        if t[:_destroy] != '1' && t[:_destroy] != 'true'
         if t[:id] && t[:language] == ''
           t[:_destroy] = '1'
         end
-        errors = errors + validate_length(t[:language], 'Language', 10)
+        errors = errors + get_errors(t[:language], 'Language', 10, '')
+          end
       end
     end
 
     # Note
     if entry_params[:notes_attributes] != nil
       entry_params[:notes_attributes].values.each do |t|
+        if t[:_destroy] != '1' && t[:_destroy] != 'true'
         if t[:id] && t[:note] == ''
           t[:_destroy] = '1'
         end
-        errors = errors + validate_length(t[:note], 'Note', LARGE_FIELD)
+        errors = errors + get_errors(t[:note], 'Note', LARGE_FIELD, '')
+          end
       end
     end
 
     # Subject
     if entry_params[:subjects_attributes] != nil
       entry_params[:subjects_attributes].values.each do |t|
+        if t[:_destroy] != '1' && t[:_destroy] != 'true'
         if t[:id] && t[:subject] == ''
           t[:_destroy] = '1'
         end
-        errors = errors + validate_length(t[:subject], 'Subject', MEDIUM_FIELD)
+        errors = errors + get_errors(t[:subject], 'Subject', MEDIUM_FIELD, '')
+          end
       end
     end
 
@@ -104,26 +112,8 @@ module Validation
 
   end
 
-  # Validate field length
-  def validate_length(field, field_name, max_length)
-    if field.length > max_length
-      return field_name + '_Length' + max_length.to_s + '|'
-    else
-      return ''
-    end
-  end
-
-  # Validate field length and mandatory field
-  def validate_length_and_mandatory_field(field, field_name, max_length)
-    if field == ''
-      return field_name + '|'
-    elsif field.length > max_length
-      return field_name + '_Length' + max_length.to_s + '|'
-    else
-      return ''
-    end
-  end
-
+  # Check for field length and mandatory field errors
+  # 'M' checks for a mandatory field
   def get_errors(field, field_name, max_length, error_type)
     # Check if it is a mandatory field
     if error_type == 'M' && field == ''
@@ -136,18 +126,4 @@ module Validation
         return ''
     end
   end
-
-  def validate_multiple_field(field_params, field, field_name, max_length, error_type)
-    if field_params != nil
-      field_params.values.each do |t|
-        if t[:_destroy] != '1' && t[:_destroy] != 'true'
-          if t[:id] && field == ''
-            t[:_destroy] = '1'
-          end
-          errors = errors + get_errors(field, field_name, max_length, error_type)
-        end
-      end
-    end
-  end
-
 end
