d5fb75e0be0be3e825f469b6c7840bc5e80348ed
Paul Young <paul.young@york.ac.uk>
2015-05-27 14:33:05 +0100

    Added a temporary read_auth file
    Former-commit-id: d097636b86d6a76aacaad82b2c887e79ce221b66

--- app/controllers/concerns/authority_list.rb
+++ app/controllers/concerns/authority_list.rb
@@ -2,7 +2,7 @@ module AuthorityList
 
   # Get authority lists
   def get_authority_lists
-    read_auth = ReadAuth.new
+    read_auth = ReadAuth2.new
     @read_language = read_auth.lookup('language')
     @read_date_type = read_auth.lookup('date-type')
     @read_folio_type = read_auth.lookup('folio-type')
--- config/initializers/active_fedora-noid.rb
+++ config/initializers/active_fedora-noid.rb
@@ -1,11 +1,11 @@
-require 'active_fedora/noid'
+#require 'active_fedora/noid'
 
-ActiveFedora::Noid.configure do |config|
+#ActiveFedora::Noid.configure do |config|
   # Specify a different template for your repository's NOID IDs
   # To use ':' the namespace must be set in Fedora (fedora-node-types.cnd)
-  config.template = 'yorkabp:.zd'
-  config.statefile = 'config/noid-minter-state'
-end
+ # config.template = 'yorkabp:.zd'
+ # config.statefile = 'config/noid-minter-state'
+#end
 
-ActiveFedora::Base.translate_uri_to_id = ActiveFedora::Noid.config.translate_uri_to_id
-ActiveFedora::Base.translate_id_to_uri = ActiveFedora::Noid.config.translate_id_to_uri
+#ActiveFedora::Base.translate_uri_to_id = ActiveFedora::Noid.config.translate_uri_to_id
+#ActiveFedora::Base.translate_id_to_uri = ActiveFedora::Noid.config.translate_id_to_uri
