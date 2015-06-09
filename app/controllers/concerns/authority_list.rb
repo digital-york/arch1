21d44e5664e05f3ed05f593a314cb291ed2fee3b
Paul Young <paul.young@york.ac.uk>
2015-06-09 17:42:32 +0100

    Working on Person 'Related Place' code (ongoing).
    Former-commit-id: 6b37023b3b0bd1370f81b910e006fe9cd0f4a3e4

--- app/controllers/concerns/authority_list.rb
+++ app/controllers/concerns/authority_list.rb
@@ -5,18 +5,31 @@ module AuthorityList
 
     # Local lists
     read_auth = ReadAuth.new
-    @language_list = read_auth.lookup('languages').collect { |l| [l["label"]] }
-    @format_list = read_auth.lookup('formats').collect { |l| [l["label"]] }
+    #@language_list = read_auth.lookup('languages').collect { |l| [l["label"]] }
+    #@format_list = read_auth.lookup('formats').collect { |l| [l["label"]] }
     @gender_list = read_auth.lookup('genders').collect { |l| [l["label"]] }
-    @date_type_list = read_auth.lookup('date-types').collect { |l| [l["label"]] }
+    #@date_type_list = read_auth.lookup('date-types').collect { |l| [l["label"]] }
 
     # Fedora lists
     @role_list = RoleTerms.new('subauthority').all
     @qualification_list = QualTerms.new('subauthority').all
     @status_list = StatusTerms.new('subauthority').all
     @place_type_list = PlaceTypeTerms.new('subauthority').all
-    @date_type_single_list = DateTypeTerms.new('subauthority').all
+    @date_type_list = DateTypeTerms.new('subauthority').all
     @date_certainty_list = CertaintyTerms.new('subauthority').all
+    @language_list = LanguageTerms.new('subauthority').all
+    @format_list = FormatTerms.new('subauthority').all
+    @date_type_single_list = SingleDateTypeTerms.new('subauthority').all
+
+    puts
+    puts "----------"
+    puts "1"
+    puts @format_list
+    puts "2"
+    puts @date_type_list
+    puts "3"
+    puts @date_type_single_list
+    puts "4"
   end
 
 end
