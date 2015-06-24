29d853c0fc9eea33f5c13e486d3b13f2b6f6c8e0
Paul Young <paul.young@york.ac.uk>
2015-06-24 09:45:45 +0100

    Subject pop-up modifications. Small changes to loadsubjects rake task.
    Former-commit-id: a2556a93ad33f35fcb4ac304802de39be18e040d

--- app/controllers/subjects_controller.rb
+++ app/controllers/subjects_controller.rb
@@ -2,10 +2,7 @@ class SubjectsController < ApplicationController
 
 def index
 
-  puts "INDEX"
-
   @subject_list = SubjectTerms.new('subauthority').all
-  #puts @subject_list
 
 end
 
@@ -14,7 +11,7 @@ def new
 end
 
 def show
-  puts "SHOW"
+
 end
 
 end
