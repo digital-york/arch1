3d1de30f02e3cdc310fe4b7cad03c6315437e3aa
Paul Young <paul.young@york.ac.uk>
2015-08-13 17:45:26 +0100

    Added some spaces inbetween methods.

--- app/qa_terms/terms.rb
+++ app/qa_terms/terms.rb
@@ -1,4 +1,5 @@
 module Terms
+
   include Qa::Authorities::WebServiceBase
 
   class FolioFaceTerms < TermsBase
@@ -6,71 +7,85 @@ module Terms
       'folio_faces'
     end
   end
+
   class CertaintyTerms < TermsBase
     def terms_list
       'certainty'
     end
   end
+
   class CurrencyTerms < TermsBase
     def terms_list
       'currencies'
     end
   end
+
   class DateRoleTerms < TermsBase
     def terms_list
       'date_roles'
     end
   end
+
   class DateTypeTerms < TermsBase
     def terms_list
       'date_types'
     end
   end
+
   class DescriptorTerms < TermsBase
     def terms_list
       'descriptors'
     end
   end
+
   class FolioTerms < TermsBase
     def terms_list
       'folio_types'
     end
   end
+
   class FormatTerms < TermsBase
     def terms_list
       'formats'
     end
   end
+
   class LanguageTerms < TermsBase
     def terms_list
       'languages'
     end
   end
+
   class EntryTypeTerms < TermsBase
     def terms_list
       'entry_types'
     end
   end
+
   class PersonRoleTerms < TermsBase
     def terms_list
       'person_roles'
     end
   end
+
   class PlaceRoleTerms < TermsBase
     def terms_list
       'place_roles'
     end
   end
+
   class PlaceTypeTerms < TermsBase
     def terms_list
       'place_types'
     end
   end
+
   class SingleDateTerms < TermsBase
     def terms_list
       'single_dates'
     end
   end
+
   class SubjectTerms < TermsBase
     def terms_list
       'subject'
