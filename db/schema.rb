6acd9030b03d2304ff39f7f6eb3907c9c8b6f90f
Paul Young <paul.young@york.ac.uk>
2015-10-07 12:42:55 +0100

    Fixed a couple of bugs. Added migration files to add and delete tables.

--- db/schema.rb
+++ db/schema.rb
@@ -10,6 +10,7 @@
 # you'll amass, the slower it'll run and the greater likelihood for issues).
 #
 # It's strongly recommended that you check this file into your version control system.
+#
 
 ActiveRecord::Schema.define(version: 20151006135919) do
 
