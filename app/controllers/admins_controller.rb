1195e0eaf6d59f70b8596615a32a08a92123ca91
Paul Young <paul.young@york.ac.uk>
2015-11-13 12:20:15 +0000

    Added 422 error page

--- app/controllers/admins_controller.rb
+++ app/controllers/admins_controller.rb
@@ -2,7 +2,6 @@ class AdminsController < ApplicationController
 
   before_filter :session_timed_out_small
 
-  # Reset concept session variables
   def index
   end
 
--- app/controllers/errors_controller.rb
+++ app/controllers/errors_controller.rb
@@ -1,10 +1,17 @@
 class ErrorsController < ApplicationController
 
   def not_found
+    log_error(exception)
     render(:status => 404)
   end
 
+  def unprocessable_entity
+    log_error(exception)
+    render(:status => 422)
+  end
+
   def internal_server_error
+    log_error(exception)
     render(:status => 500)
   end
 end
