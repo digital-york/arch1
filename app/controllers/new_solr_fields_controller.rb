82a10e19b0acb1e68595666ffa02b2c83284fc26
Julie Allinson <j.allinson@gmail.com>
2016-01-28 00:43:13 +0000

    new solr field for label string

--- app/controllers/new_solr_fields_controller.rb
+++ app/controllers/new_solr_fields_controller.rb
@@ -4,18 +4,22 @@ class NewSolrFieldsController < ApplicationController
   # Note that this code is called from initializers/active_fedora.rb and overrides the to_solr active_fedora method
   # '_search' fields are added for searching in the interface and are lowercase versions of the actual terms
   # '_new_tesim' fields are created from ids, e.g. for language, so that solr doesn't have to join documents
+  # '_label_ssim' fields provide the string and are used for facet counts
   # 'search' is a new fieldType which is defined in the solr schema.xml
   # 'new_tesim' already exists as fieldType 'tesim' in schema.xml
+  # 'label_ssim' already exists as fieldType 'ssim' in schema.xml
   def modify_sdoc(sdoc)
 
     begin
 
       entry_type_new = get_preflabel_array(sdoc['entry_type_tesim'])
       sdoc['entry_type_new_tesim'] = entry_type_new
+      sdoc['entry_type_label_ssim'] = entry_type_new
       sdoc['entry_type_search'] = array_to_lowercase(entry_type_new)
 
       section_type_new = get_preflabel_array(sdoc['section_type_tesim'])
       sdoc['section_type_new_tesim'] = section_type_new
+      sdoc['section_type_label_ssim'] = section_type_new
       sdoc['section_type_search'] = array_to_lowercase(section_type_new)
 
       sdoc['summary_search'] = array_to_lowercase(sdoc['summary_tesim'])
@@ -24,11 +28,13 @@ class NewSolrFieldsController < ApplicationController
 
       language_new = get_preflabel_array(sdoc['language_tesim'])
       sdoc['language_new_tesim'] = language_new
+      sdoc['language_label_ssim'] = language_new
       sdoc['language_search'] = array_to_lowercase(language_new)
 
       subject_new = get_preflabel_array(sdoc['subject_tesim'])
       sdoc['subject_new_tesim'] = subject_new
       sdoc['subject_search'] = array_to_lowercase(subject_new)
+      sdoc['subject_label_ssim'] = subject_new
 
       sdoc['note_search'] = array_to_lowercase(sdoc['note_tesim'])
 
@@ -40,14 +46,17 @@ class NewSolrFieldsController < ApplicationController
 
       place_name_authority_new = get_preflabel_array(sdoc['place_same_as_tesim'])
       sdoc['place_same_as_new_tesim'] = place_name_authority_new
+      sdoc['place_same_as_label_ssim'] = place_name_authority_new
       sdoc['place_same_as_search'] = array_to_lowercase(place_name_authority_new)
 
       place_role_new = get_preflabel_array(sdoc['place_role_tesim'])
       sdoc['place_role_new_tesim'] = place_role_new
+      sdoc['place_role_label_ssim'] = place_role_new
       sdoc['place_role_search'] = array_to_lowercase(place_role_new)
 
       place_type_new = get_preflabel_array(sdoc['place_type_tesim'])
       sdoc['place_type_new_tesim'] = place_type_new
+      sdoc['place_type_label_ssim'] = place_type_new
       sdoc['place_type_search'] = array_to_lowercase(place_type_new)
 
       sdoc['place_note_search'] = array_to_lowercase(sdoc['place_note_tesim'])
@@ -56,14 +65,17 @@ class NewSolrFieldsController < ApplicationController
 
       person_name_authority_new = get_preflabel_array(sdoc['person_same_as_tesim'])
       sdoc['person_same_as_new_tesim'] = person_name_authority_new
+      sdoc['person_same_as_label_ssim'] = person_name_authority_new
       sdoc['person_same_as_search'] = array_to_lowercase(person_name_authority_new)
 
       person_role_new = get_preflabel_array(sdoc['person_role_tesim'])
       sdoc['person_role_new_tesim'] = person_role_new
+      sdoc['person_role_label_ssim'] = person_role_new
       sdoc['person_role_search'] = array_to_lowercase(person_role_new)
 
       person_descriptor_new = get_preflabel_array(sdoc['person_descriptor_tesim'])
       sdoc['person_descriptor_new_tesim'] = person_descriptor_new
+      sdoc['person_descriptor_label_ssim'] = person_descriptor_new
       sdoc['person_descriptor_search'] = array_to_lowercase(person_descriptor_new)
 
       sdoc['person_descriptor_as_written_search'] = array_to_lowercase(sdoc['person_descriptor_tesim'])
@@ -77,6 +89,7 @@ class NewSolrFieldsController < ApplicationController
       # dates
       date_role_new = get_preflabel_array(sdoc['date_role_tesim'])
       sdoc['date_role_new_tesim'] = date_role_new
+      sdoc['date_role_label_ssim'] = date_role_new
       sdoc['date_role_search'] = array_to_lowercase(date_role_new)
 
       sdoc['date_note_search'] = array_to_lowercase(sdoc['date_note_tesim'])
