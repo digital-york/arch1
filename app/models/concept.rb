330c1952f9a4f4506eeb9b9ac2c80d1c474474ec
root <root@opal.york.ac.uk>
2016-02-03 20:24:55 +0000

    concept model changes for broader/narrower fix

--- app/models/concept.rb
+++ app/models/concept.rb
@@ -5,8 +5,8 @@ class Concept < ActiveFedora::Base
   belongs_to :concept_scheme, predicate: ::RDF::SKOS.inScheme
 
   # Use only for Broader, Narrower will be added by default
-  #has_and_belongs_to_many :broader, class_name: 'Concept', predicate: ::RDF::SKOS.broader, inverse_of: :narrower
-  #has_and_belongs_to_many :narrower, class_name: 'Concept', predicate: ::RDF::SKOS.narrower, inverse_of: :broader
+  has_and_belongs_to_many :broader, class_name: 'Concept', predicate: ::RDF::SKOS.broader, inverse_of: :narrower
+  has_and_belongs_to_many :narrower, class_name: 'Concept', predicate: ::RDF::SKOS.narrower, inverse_of: :broader
 
   property :definition, predicate: ::RDF::SKOS.definition, multiple: false do |index|
     index.as :stored_searchable
@@ -17,7 +17,7 @@ class Concept < ActiveFedora::Base
   end
 
   # change this to has and belongs to many as doesn't save properly in latest version of AF
-#=begin
+=begin
   property :broader, predicate: ::RDF::SKOS.broader, multiple: true do |index|
     index.as :stored_searchable
   end
@@ -25,7 +25,7 @@ class Concept < ActiveFedora::Base
   property :narrower, predicate: ::RDF::SKOS.narrower, multiple: true do |index|
     index.as :stored_searchable
   end
-#=end
+=end
 
   # there is more skos we could add ...
 
