3b70d10c464188111376b82924062e0ac478b05d
Sebastian Pałucha <sebastian.palucha@york.ac.uk>
2020-07-04 18:04:34 +0100

    Make rspec running
    Configure rspce
    - run db:migration RAILS_ENV=test
    - configure some Rails v5 deprecation warning running
    
    Update ingest helper test
    - tidy up comments
    - correct method name, add context, test happy/unhapy paths
    - use rails environment as set at .env

--- spec/helpers/ingest/register_helper_spec.rb
+++ spec/helpers/ingest/register_helper_spec.rb
@@ -1,20 +1,17 @@
-require 'spec_helper'
-require_relative "../../../app/helpers/ingest/register_helper"
+require 'rails_helper'
 
-# RSpec.describe MyMath do
-#     it 'returns five' do
-#         expect(MyMath.number).to eq(5)
-#     end
-# end
+RSpec.describe Ingest::RegisterHelper do
 
-#module Ingest
-#     module Register
-        describe Ingest::RegisterHelper do
-
-            it "has get_register_id" do
-                id = Ingest::RegisterHelper.get_register_ids('Abp Reg 1A: Walter de Gray (1215-1255) Major roll 1225-1235')
-                expect(id[0]).to eq('dz010q943')
-            end
+    context 'When s_get_register_id search Solr index' do
+        it 'returns id for existing register' do
+            id = Ingest::RegisterHelper.s_get_register_ids('Abp Reg 1A: Walter de Gray (1215-1255) Major roll 1225-1235')
+            expect(id[0]).to eq('dz010q943')
         end
-#     end
-#end
+
+        it 'returns empty [] for dumy register' do
+            id = Ingest::RegisterHelper.s_get_register_ids('Dummy register')
+            expect(id.class).to eq(Array)
+        end
+    end
+
+end
