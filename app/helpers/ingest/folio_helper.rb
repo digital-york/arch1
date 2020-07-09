6e32fb8c70fc7a67c971b50e0c432b9b60c87de4
Frank Feng <frank.feng@york.ac.uk>
2020-07-09 16:55:36 +0100

    get ar folio id from register_name, folio_id, folio_side

--- app/helpers/ingest/folio_helper.rb
+++ app/helpers/ingest/folio_helper.rb
@@ -18,5 +18,21 @@ module Ingest
                 ids = r['id']
             end
         end
+
+        # get archbishop register folio id
+        # input: register, folio, side
+        def self.s_get_ar_folio_id(register_name, folio_id, folio_side)
+            prefix = 'Abp'
+            id = nil
+            # Convert folio info from:
+            #   Register 7	132	(verso)
+            # to:
+            #   Abp Reg 7 f.132 (recto)
+            folio_label = "#{prefix} #{register_name.gsub('Register', 'Reg')} f.#{folio_id} #{folio_side}"
+            SolrQuery.new.solr_query("preflabel_tesim:\"#{folio_label}\"")['response']['docs'].map do |r|
+                id = r['id']
+            end
+            id
+        end
     end
 end
