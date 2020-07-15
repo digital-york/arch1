603fbb3c7a8d3b597b5eff49a36a3c490323f7f2
Frank Feng <frank.feng@york.ac.uk>
2020-07-15 17:18:46 +0100

    add create_register method to RegisterHelper

--- app/helpers/ingest/ordered_collection_helper.rb
+++ app/helpers/ingest/ordered_collection_helper.rb
@@ -1,7 +1,9 @@
 module Ingest
     module OrderedCollectionHelper
         # create a new ordered collection
-        # return the id of newly created ordered collection
+        # return the newly created ordered collection
+        # e.g.
+        #   c = Ingest::OrderedCollectionHelper.create_ordered_colleciton('Test collection')
         def self.create_ordered_colleciton(pref_label)
             c = OrderedCollection.new
             c.rdftype = c.add_rdf_types
@@ -11,7 +13,5 @@ module Ingest
 
             c
         end
-
-
     end
 end
--- app/helpers/ingest/register_helper.rb
+++ app/helpers/ingest/register_helper.rb
@@ -10,5 +10,22 @@ module Ingest
                 ids = r['id']
             end
         end
+
+        # create a new register
+        # return the newly created register
+        # e.g.
+        #   Ingest::RegisterHelper.create_register('Test register', 'Test register','9z903019d', 'https://dlib.york.ac.uk/yodl/api/resource/york:799266/assets/thumbnail.jpg')
+        def self.create_register(pref_label, reg_id, ordered_collection_id, thumbnail_url)
+            r = Register.new
+            r.rdftype               = r.add_rdf_types
+            r.preflabel             = pref_label
+            r.reg_id                = reg_id
+            r.thumbnail_url         = thumbnail_url
+            r.ordered_collection_id = ordered_collection_id
+
+            r.save
+
+            r
+        end
     end
 end
