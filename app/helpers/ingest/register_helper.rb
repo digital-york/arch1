6f9fdbd4ff3dab55eff34af5faf9e6f9f7c577ce
Frank Feng <frank.feng@york.ac.uk>
2020-07-22 10:27:18 +0100

    add create_register method

--- app/helpers/ingest/register_helper.rb
+++ app/helpers/ingest/register_helper.rb
@@ -15,13 +15,17 @@ module Ingest
         # return the newly created register
         # e.g.
         #   Ingest::RegisterHelper.create_register('Test register', 'Test register','9z903019d', 'https://dlib.york.ac.uk/yodl/api/resource/york:799266/assets/thumbnail.jpg')
+        #
+        # TO INVESTIGATE: register_id/list_source
+        #
+        #
         def self.create_register(pref_label, reg_id, ordered_collection_id, thumbnail_url)
             r = Register.new
             r.rdftype               = r.add_rdf_types
             r.preflabel             = pref_label
             r.reg_id                = reg_id
             r.thumbnail_url         = thumbnail_url
-            r.ordered_collection_id = ordered_collection_id
+            r.ordered_collection    = OrderedCollection.find(ordered_collection_id)
 
             r.save
 
