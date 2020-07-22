57168c3a7432f2ccaa1ed59de7a96d30d4128f60
Frank Feng <frank.feng@york.ac.uk>
2020-07-22 16:19:06 +0100

    get section type working

--- app/helpers/ingest/borthwick_entry_builder.rb
+++ app/helpers/ingest/borthwick_entry_builder.rb
@@ -3,30 +3,37 @@ module Ingest
     class BorthwickEntryBuilder
 
         def self.build_entry(borthwick_entry_row)
-            e = Entry.new
-
-            # add entry rdf types
-            e.rdftype = e.add_rdf_types
+            # register_name
+            register_name = borthwick_entry_row.register
 
             # Link entry to folio
             folio_id = Ingest::FolioHelper.s_get_ar_folio_id(borthwick_entry_row.register,
                                                              borthwick_entry_row.folio_no,
                                                              borthwick_entry_row.folio_side)
             if folio_id.nil?
+                puts 'cannot find folio via id: ' + folio_id
                 return nil
             end
-            e.folio = Folio.find(folio_id)
 
             # entry number
-            e.entry_no = borthwick_entry_row.entry_no.to_s
+            entry_no = borthwick_entry_row.entry_no.to_s
             # store entry_no into former_id as well
-            e.former_id = [borthwick_entry_row.entry_no.to_s]
+            former_id = [borthwick_entry_row.entry_no.to_s]
+
+            # entry types
+            entry_types = []
+            entry_types << borthwick_entry_row.entry_type1 unless borthwick_entry_row.entry_type1.blank?
+            entry_types << borthwick_entry_row.entry_type2 unless borthwick_entry_row.entry_type2.blank?
+            entry_types << borthwick_entry_row.entry_type3 unless borthwick_entry_row.entry_type3.blank?
 
             # language
             # Q: NO language column in Reg7132v-entry-6-155-York-EHW.xlsx
+            languages = []
+            languages << borthwick_entry_row.language1 unless borthwick_entry_row.language1.blank?
+            languages << borthwick_entry_row.language2 unless borthwick_entry_row.language2.blank?
 
             # section type
-            e.section_type = Ingest::AuthorityHelper.s_get_section_type_object_ids([borthwick_entry_row.section_type])
+            section_type = Ingest::AuthorityHelper.s_get_section_type_object_ids([borthwick_entry_row.section_type])
 
             # subject
             subjects = []
@@ -34,32 +41,31 @@ module Ingest
             subjects << borthwick_entry_row.subject2 unless borthwick_entry_row.subject2.blank?
             subjects << borthwick_entry_row.subject3 unless borthwick_entry_row.subject3.blank?
             subjects << borthwick_entry_row.subject4 unless borthwick_entry_row.subject4.blank?
-            e.subject = subjects
 
             # is_referenced_by
-            e.is_referenced_by = [borthwick_entry_row.referenced_by] unless borthwick_entry_row.referenced_by.blank?
+            referenced_by = [borthwick_entry_row.referenced_by] unless borthwick_entry_row.referenced_by.blank?
 
             # note
-            e.note = [borthwick_entry_row.note] unless borthwick_entry_row.note.blank?
+            note = [borthwick_entry_row.note] unless borthwick_entry_row.note.blank?
 
             # editorial_note
             # no editorial_note field?
 
-            # Continued folio no and side
-            unless borthwick_entry_row.continues_folio_no.blank? or borthwick_entry_row.continues_folio_side.blank?
-                continuted_folio_id = Ingest::FolioHelper.s_get_ar_folio_id(
-                                                        borthwick_entry_row.register,
-                                                        borthwick_entry_row.folio_no,
-                                                        borthwick_entry_row.folio_side)
-                continuted_folio = Folio.find(continuted_folio_id)
-                e.continues_on = continuted_folio unless continuted_folio.nil?
-            end
-
-            e.save
-            e.folio.entries += [e]
-            e.folio.save
-
-            e
+            e = Ingest::EntryHelper.create_entry(
+                register_name,
+                folio_id,
+                entry_no,
+                entry_types,
+                former_id,
+                languages,
+                section_type,
+                subjects,
+                referenced_by,
+                note,
+                borthwick_entry_row.continues_folio_no,
+                borthwick_entry_row.continues_folio_side
+            )
+            puts "Created Entry: #{e.id}"
         end
 
     end
--- app/helpers/ingest/entry_helper.rb
+++ app/helpers/ingest/entry_helper.rb
@@ -10,5 +10,73 @@ module Ingest
                 ids = r['id']
             end
         end
+
+        # Ingest::FolioHelper.create_entry
+        def self.create_entry(register_name,
+                              folio_id,
+                              entry_no,
+                              entry_types,
+                              former_id,
+                              languages,
+                              section_type,
+                              subjects,
+                              referenced_by,
+                              note,
+                              continues_folio_no,
+                              continues_folio_side
+                              )
+            #e = Entry.new
+            e = Entry.find('2b88qc56f')
+
+            # add entry rdf types
+            e.rdftype = e.add_rdf_types
+
+            # link entry to folio
+            e.folio = Folio.find(folio_id)
+
+            # entry number
+            e.entry_no = entry_no
+
+            # store former_id/entry_no into former_id
+            e.former_id = former_id
+
+            e.entry_type = Ingest::AuthorityHelper.s_get_entry_type_object_ids(entry_types)
+
+            # language
+            # Q: NO language column in Reg7132v-entry-6-155-York-EHW.xlsx
+            e.language = Ingest::AuthorityHelper.s_get_language_object_ids(languages) unless languages.blank?
+
+            # section type
+            e.section_type = section_type
+
+            # subject
+            e.subject = subjects unless subjects.blank?
+
+            # is_referenced_by
+            # Find the authority object by the referenced_by text
+            #e.is_referenced_by = [referenced_by] unless referenced_by.blank?
+
+            # note
+            e.note = [note] unless note.blank?
+
+            # editorial_note
+            # no editorial_note field?
+
+            # Continued folio no and side
+            unless continues_folio_no.blank? or continues_folio_side.blank?
+                continuted_folio_id = Ingest::FolioHelper.s_get_ar_folio_id(
+                                            register_name,
+                                            continues_folio_no,
+                                            continues_folio_side)
+                continuted_folio = Folio.find(continuted_folio_id)
+                e.continues_on = continuted_folio unless continuted_folio.nil?
+            end
+
+            e.save
+#            e.folio.entries += [e]
+#            e.folio.save
+
+            e
+        end
     end
 end
