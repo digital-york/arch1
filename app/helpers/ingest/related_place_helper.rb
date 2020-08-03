2700d4b33a9050ff4b7682655f9300c7e7498295
Frank Feng <frank.feng@york.ac.uk>
2020-08-03 12:11:03 +0100

    add additional checks of nil

--- app/helpers/ingest/related_place_helper.rb
+++ app/helpers/ingest/related_place_helper.rb
@@ -9,11 +9,11 @@ module Ingest
             rp = RelatedPlace.new
 
             rp.rdftype          = rp.add_rdf_types
-            rp.place_as_written = [place_as_written]    # string
-            rp.place_same_as    = place_name_authority  # reference
-            rp.place_role       = place_roles           # reference
-            rp.place_type       = place_types           # reference
-            rp.place_note       = place_notes           # string
+            rp.place_as_written = [place_as_written] unless place_as_written.blank?       # string
+            rp.place_same_as    = place_name_authority unless place_name_authority.blank? # reference
+            rp.place_role       = place_roles unless place_roles.blank?                   # reference
+            rp.place_type       = place_types unless place_types.blank?                   # reference
+            rp.place_note       = place_notes unless place_notes.blank?                   # string
 
             rp.save
 
