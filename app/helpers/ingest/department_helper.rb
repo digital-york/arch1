a437ad0a9b6c0627f6297066e06348336a0d88ab
Frank Feng <frank.feng@york.ac.uk>
2021-04-26 11:37:02 +0100

    check existing department before creating new department

--- app/helpers/ingest/department_helper.rb
+++ app/helpers/ingest/department_helper.rb
@@ -3,19 +3,26 @@ module Ingest
 
         # search department's id by the given department label
         # The prefix 's_' is the convention here, means retrieving data from Solr
+        # Ingest::DepartmentHelper.s_get_department_id('C')
         def self.s_get_department_id(department_label)
-            ids = []
+            id = ''
             begin
                 SolrQuery.new.solr_query("has_model_ssim:\"Department\" AND preflabel_tesim:\"#{department_label}\"")['response']['docs'].map do |r|
-                    ids = r['id']
+                    id = r['id']
                 end
             rescue
             end
-            ids
+            id
         end
 
-        # Ingest::DepartmentHelper.create_department('Test Series')
+        # Ingest::DepartmentHelper.create_department('C','Chancery')
         def self.create_department(department_label, department_desc)
+
+            existing_department_id = Ingest::DepartmentHelper.s_get_department_id(department_label)
+            unless existing_department_id.blank?
+                return Department.find(existing_department_id)
+            end
+
             d = Department.new
             d.rdftype = d.add_rdf_types
             d.preflabel = department_label
@@ -25,4 +32,5 @@ module Ingest
 
             d
         end
+    end
 end
