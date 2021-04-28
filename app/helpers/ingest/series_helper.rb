4152e68962ceb9ac8ae99ff30256612b8951fc1b
Frank Feng <frank.feng@york.ac.uk>
2021-04-28 10:25:09 +0100

    add method for creating series and search series id by department id and label

--- app/helpers/ingest/series_helper.rb
+++ app/helpers/ingest/series_helper.rb
@@ -2,28 +2,34 @@ module Ingest
     module SeriesHelper
         # search series' id by the given document reference
         # The prefix 's_' is the convention here, means retrieving data from Solr
-        def self.s_get_series_id(document_reference)
-            ids = []
+        # Ingest::SeriesHelper.s_get_series_id('6108vp64c', 'C1')
+        def self.s_get_series_id(department_id, series_label)
+            id = ''
             begin
-                series_name = document_reference.split('/')[0].split(' ')[1]
-                SolrQuery.new.solr_query("has_model_ssim:\"Series\" AND preflabel_tesim:\"#{series_name}\"")['response']['docs'].map do |r|
-                    ids = r['id']
+                SolrQuery.new.solr_query("has_model_ssim:\"Series\" AND preflabel_tesim:\"#{series_label}\" AND isPartOf_ssim:\"#{department_id}\"")['response']['docs'].map do |r|
+                    id = r['id']
                 end
             rescue
             end
-            ids
+            id
         end
 
-        # Ingest::SeriesHelper.create_series('Test Series', 'xx')
-        def self.create_folio(series_label, series_desc, department_id)
+        # Ingest::SeriesHelper.create_series('C1', 'Early Chancery Proceedings', '6108vp64c')
+        def self.create_series(series_label, series_desc, department_id)
+            # trying to find if the series has been created first
+            s_id = Ingest::SeriesHelper.s_get_series_id(department_id, series_label)
+
+            return Series.find(s_id) unless s_id.blank?
+
             s = Series.new
             s.rdftype = s.add_rdf_types
             s.preflabel = series_label
-            s.descritipn = series_desc
+            s.description = series_desc
             s.department = Department.find(department_id)
 
             s.save
 
             s
         end
+    end
 end
