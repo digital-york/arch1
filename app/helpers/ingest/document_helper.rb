e13019b37e1e5908edaa23d61276353e7d3fa380
Frank Feng <frank.feng@york.ac.uk>
2021-04-29 10:44:51 +0100

    draft version of ingest without any authorities

---
+++ app/helpers/ingest/document_helper.rb
@@ -0,0 +1,92 @@
+require 'logger'
+
+module Ingest
+    module DocumentHelper
+        # search department's id by the given document reference
+        # The prefix 's_' is the convention here, means retrieving data from Solr
+        def self.s_get_department_label(document_reference)
+            reference.split(' ')[0]
+        end
+
+        # search series's id by the given document reference
+        def self.s_get_series_label(document_reference)
+            reference.split('/')[0].gsub(" ", "")
+        end
+
+        # find Document by series id and reference.
+        # pry(main)> Ingest::EntryHelper.s_find_document('1257b485h', 'C 81/1786/43')
+        # => "2b88qc56f"
+        def self.s_find_document(series_id, reference)
+            document_id = nil
+
+            unless series_id.nil? or reference.nil?
+                query = "has_model_ssim:Document AND series_ssim:#{series_id} AND reference_tesim:#{reference}"
+                SolrQuery.new.solr_query(query)['response']['docs'].map do |r|
+                    document_id = r['id']
+                end
+            end
+
+            document_id
+        end
+
+        # Ingest::DocumentHelper.create_document
+        def self.create_or_update_document(
+            allow_edit,
+            series_id,
+            reference,
+            publication,
+            summary,
+            entry_date_note,
+            note,
+            document_type,
+            date_of_document,
+            place_of_dating,
+            language,
+            subject,
+            addressee,
+            sender,
+            person,
+            place)
+            log = Logger.new "log/document_helper.log"
+
+            document_id = s_find_document(series_id, reference)
+
+            if document_id.nil?
+                d = Document.new
+            else
+                d = Document.find(document_id)
+                puts '  found document ' + document_id
+                log.info '  found document ' + document_id
+                if allow_edit==false
+                    return d
+                end
+            end
+
+            # add document rdf types
+            d.rdftype = d.add_rdf_types
+            # link document to Series
+            d.series = Series.find(series_id)
+            d.reference = reference
+            d.publication = publication
+            d.summary = summary
+            d.entry_date_note = entry_date_note
+            d.note = note
+            d.document_type = document_type
+
+            # d.date_of_document = date_of_document
+
+            d.language = Ingest::AuthorityHelper.s_get_language_object_ids(language) unless language.blank?
+            d.subject = subject unless subject.blank?
+            # d.place_of_dating = place_of_dating
+            # d.addressee = addressee
+            # d.sender = sender
+            # d.person = person
+            # d.place = place
+
+            d.save
+            puts "  Created/updated Document: #{d.id}" unless d.nil?
+            log.info "  Created/updated Document: #{d.id}" unless d.nil?
+            d
+        end
+    end
+end
---
+++ app/helpers/ingest/tna_document_builder.rb
@@ -0,0 +1,97 @@
+require 'logger'
+
+module Ingest
+    # This class build a Document from TnaDocumentRow object
+    class TnaDocumentBuilder
+
+        def self.build_document(tna_document_row, allow_edit=false)
+            log = Logger.new "log/tna_document_builder.log"
+
+            # document reference
+            reference = tna_document_row.reference
+
+            department_label = reference.split(' ')[0]
+            department_id = Ingest::DepartmentHelper.s_get_department_id(department_label)
+            if department_id.nil?
+                puts 'cannot find department for: ' + reference
+                log.info 'cannot find department for: ' + reference
+                return nil
+            end
+
+            series_label = reference.split('/')[0].gsub(" ", "")
+            series_id = Ingest::SeriesHelper.s_get_series_id(department_id, series_label)
+            if series_id.nil?
+                puts 'cannot find series for: ' + reference
+                log.info 'cannot find series: ' + reference
+                return nil
+            end
+
+            # publications
+            publications = []
+            publications = [tna_document_row.publication] unless tna_document_row.publication.blank?
+
+            summary = tna_document_row.summary unless tna_document_row.summary.blank?
+
+            entry_date_notes = []
+            entry_date_notes = [tna_document_row.entry_date_note] unless tna_document_row.entry_date_note.blank?
+
+            notes = []
+            notes = [tna_document_row.note] unless tna_document_row.note.blank?
+
+            document_types = []
+            document_types = [tna_document_row.document_type] unless tna_document_row.document_type.blank?
+
+            date_of_documents = []
+            date_of_documents = [tna_document_row.date_of_document] unless tna_document_row.date_of_document.blank?
+
+            place_of_dating = tna_document_row.place_of_dating unless tna_document_row.place_of_dating.blank?
+
+            languages = []
+            if tna_document_row.language.blank?
+                languages << 'Latin'
+            else
+                languages << tna_document_row.language
+            end
+
+            # subject
+            subjects = []
+            subjects << tna_document_row.subject unless tna_document_row.subject.blank?
+
+            # addressees
+            addressees = []
+            addressees << tna_document_row.addressee unless tna_document_row.addressee.blank?
+
+            # senders
+            senders = []
+            senders << tna_document_row.sender unless tna_document_row.sender.blank?
+
+            # person
+            persons = []
+            persons << tna_document_row.person unless tna_document_row.person.blank?
+
+            # place
+            places = []
+            places << tna_document_row.place unless tna_document_row.place.blank?
+
+            d = Ingest::DocumentHelper.create_or_update_document(
+                allow_edit,
+                series_id,
+                reference,
+                publications,
+                summary,
+                entry_date_notes,
+                notes,
+                document_types,
+                date_of_documents,
+                place_of_dating,
+                languages,
+                subjects,
+                addressees,
+                senders,
+                persons,
+                places
+            )
+        end
+
+    end
+end
