47f510b436c8a2da65a36eccaa209959df9ba733
Frank Feng <frank.feng@york.ac.uk>
2021-05-19 11:31:32 +0100

    add TnaPlaceHelper

---
+++ app/helpers/ingest/tna_place_helper.rb
@@ -0,0 +1,20 @@
+module Ingest
+    module TnaPlaceHelper
+        # Ingest::TnaPlaceHelper.create_tna_place
+        def self.create_tna_place(place_as_written,
+            place_name_authority,
+            place_roles)
+            tna_place = TnaPlace.new
+
+            tna_place.rdftype = tna_place.add_rdf_types
+            tna_place.place_as_written = [place_as_written] unless place_as_written.blank? # string
+            tna_place.place_same_as = place_name_authority unless place_name_authority.blank? # reference
+            tna_place.place_role = place_roles unless place_roles.blank? # reference
+
+
+            tna_place.save
+
+            tna_place
+        end
+    end
+end
