f300071f90ea852fde3bc58ccf719bdcead58500
Frank Feng <frank.feng@york.ac.uk>
2021-05-26 10:59:00 +0100

    if a place name starts with ?, then add place note: identification uncertain

--- app/helpers/ingest/tna_place_desc.rb
+++ app/helpers/ingest/tna_place_desc.rb
@@ -1,6 +1,6 @@
 module Ingest
     class TnaPlaceDesc
-        attr_accessor :place_as_written, :place_name, :place_role, :place_type, :county, :country
+        attr_accessor :place_as_written, :place_name, :place_role, :place_type, :county, :country, :place_note
 
         def initialize(place_as_written,
                        place_name,
@@ -9,7 +9,16 @@ module Ingest
                        county,
                        country)
             @place_as_written    = place_as_written
-            @place_name = place_name
+            # add 'Identification uncertain' to place note field if the place name starting with '?'
+            if place_name.starts_with? '? '
+                @place_name = place_name[2..place_name.length]
+                @place_note = 'identification uncertain'
+            elsif place_name.starts_with? '?'
+                @place_name = place_name[1..place_name.length]
+                @place_note = 'identification uncertain'
+            else
+                @place_name = place_name
+            end
             @place_role = place_role
             @place_type = place_type
             @county = county
